83e5fff3c6a062384d6299626fcfa2dc545e7295
pryans <spraguep@gmail.com>
2019-11-01 12:07:07 -0400

    fixes/comments

--- Source/Scripts/autocheat.lua
+++ Source/Scripts/autocheat.lua
@@ -1,6 +1,10 @@
-System.ExecuteCommand("cheat:logInfo(\"running auotcheat.lua\")")
+cheat:logInfo("running auotcheat.lua")
 
--- is a command
+-- a comment
+
+-- to run a console command from a lua script you must use System.ExecuteCommand("")
+
+System.ExecuteCommand("cheat_eval cheat:logInfo(\"autocheat.lua test\")")
 
 --System.ExecuteCommand("cheat_eval cheat:logSetLevel(cheat.g_cheat_log_level_debug)")
 --System.ExecuteCommand("cheat_set_bow_reticle enable:true")
