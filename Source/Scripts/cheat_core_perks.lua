d3b204739d094f48a6ef9faa8f66073ac3ccb7d1
wilecoyote <wile_coyote@aon.at>
2022-04-01 23:20:15 +0200

    Removed duplicate entry for Brittle Bones

--- Source/Scripts/cheat_core_perks.lua
+++ Source/Scripts/cheat_core_perks.lua
@@ -116,34 +116,33 @@ function cheat:cheat_add_all_perks(line)
   local exclude, excludeErr = cheat:argsGet(args, 'exclude')
   
   local excludes = {}
-  excludes["fa299718-b1eb-4664-8769-25f82fb95de9"] = true --LimitSprint
-  excludes["12c75fff-d00d-4cb0-8c27-4a8e4838dc14"] = true --test_dummy_perk
-  excludes["519db599-76d4-4703-8c31-486fae00e473"] = true --test_recipe
-  excludes["a51bfbf1-f60b-40d2-ae3e-830127523862"] = true --test_subperk
-  excludes["e97c3ca4-04bc-4dfc-bd80-8002280c7c14"] = true --test_metaperk
-  excludes["775dcec-5dcd-48e7-810d-1fc97e4e203e"] = true--Combo srt 04 cut mace OBSOLETE
-  excludes["b74821b-9312-4e76-9c22-d2a38ba9dd06"] = true--Combo srt 05 diagonalstrike mace OBSOLETE"
-  excludes["b89f2c7-e5f3-415f-aec7-32d7655a94ba"] = true--Combo srt 03 pommelstrike axe OBSOLETE"
-  excludes["9ec2cf8-6e0b-4468-a7d1-7e538b676467"] = true--Combo srt 05 diagonalstrike axe OBSOLETE"
-  excludes["974b67f-0785-43ce-b51f-9779ecf42fb1"] = true--Combo srt 03 pommelstrike shs OBSOLETE"
-  excludes["574c37d-08af-4576-bcfd-29ccfa79e5ba"] = true--Combo srt 03 pommelstrike mace OBSOLETE"
-  excludes["8d9d23f-e293-4980-abd4-a3046b929771"] = true--Combo srt 04 cut OBSOLETE"
-  excludes["3f6c0eb-1b03-46ad-89f5-7eda9e4ab548"] = true--Combo srt 04 cut axe OBSOLETE"
-  excludes["80825cd9-7d7b-440f-aa57-75807e83aed9"] = true --Always drunk
+  excludes["80825cd9-7d7b-440f-aa57-75807e83aed9"] = true -- Always drunk
+  excludes["fa299718-b1eb-4664-8769-25f82fb95de9"] = true -- LimitSprint
+  excludes["12c75fff-d00d-4cb0-8c27-4a8e4838dc14"] = true -- test_dummy_perk
+  excludes["519db599-76d4-4703-8c31-486fae00e473"] = true -- test_recipe
+  excludes["a51bfbf1-f60b-40d2-ae3e-830127523862"] = true -- test_subperk
+  excludes["e97c3ca4-04bc-4dfc-bd80-8002280c7c14"] = true -- test_metaperk
+  excludes["775dcec-5dcd-48e7-810d-1fc97e4e203e"] = true -- Combo srt 04 cut mace OBSOLETE
+  excludes["b74821b-9312-4e76-9c22-d2a38ba9dd06"] = true -- Combo srt 05 diagonalstrike mace OBSOLETE"
+  excludes["b89f2c7-e5f3-415f-aec7-32d7655a94ba"] = true -- Combo srt 03 pommelstrike axe OBSOLETE"
+  excludes["9ec2cf8-6e0b-4468-a7d1-7e538b676467"] = true -- Combo srt 05 diagonalstrike axe OBSOLETE"
+  excludes["974b67f-0785-43ce-b51f-9779ecf42fb1"] = true -- Combo srt 03 pommelstrike shs OBSOLETE"
+  excludes["574c37d-08af-4576-bcfd-29ccfa79e5ba"] = true -- Combo srt 03 pommelstrike mace OBSOLETE"
+  excludes["8d9d23f-e293-4980-abd4-a3046b929771"] = true -- Combo srt 04 cut OBSOLETE"
+  excludes["3f6c0eb-1b03-46ad-89f5-7eda9e4ab548"] = true -- Combo srt 04 cut axe OBSOLETE"
   
   -- charlitoti
   -- here is an extension of the excludes perk to exclude aswell the hardcore negative perks:
-  excludes["fbedb426-410c-4614-952a-1086b6f6554f"] = true
-  excludes["37433f7b-9c2e-48e2-bce7-af8d34b403c8"] = true
-  excludes["4d51ba41-2c10-4281-9308-fcfed1fe0276"] = true
-  excludes["5ef31fc4-244e-40ac-b088-03e5730ff5c1"] = true
-  excludes["6402905d-6cfa-4666-80bf-2a70b0b82bd1"] = true
-  excludes["aa725966-98eb-4db2-8cd5-ad3d43b13f14"] = true
-  excludes["b59a2f39-faf4-4a1d-88c2-c059dadc6abb"] = true
-  excludes["b9aa28f1-ccbb-4c0c-9718-c218f01d749b"] = true
-  excludes["ce2fe289-4c26-45c0-803b-32627d288765"] = true
-  excludes["d2105041-120b-4c06-8e61-1948a5fdf65d"] = true
-  excludes["fbedb426-410c-4614-952a-1086b6f6554f"] = true
+  excludes["37433f7b-9c2e-48e2-bce7-af8d34b403c8"] = true -- Nightmares
+  excludes["5ef31fc4-244e-40ac-b088-03e5730ff5c1"] = true -- Claustrophobia
+  excludes["6402905d-6cfa-4666-80bf-2a70b0b82bd1"] = true -- Haemophilia
+  excludes["aa725966-98eb-4db2-8cd5-ad3d43b13f14"] = true -- Numbskull
+  excludes["b9aa28f1-ccbb-4c0c-9718-c218f01d749b"] = true -- Consumption
+  excludes["b59a2f39-faf4-4a1d-88c2-c059dadc6abb"] = true -- Shakes
+  excludes["ce2fe289-4c26-45c0-803b-32627d288765"] = true -- Tapeworm
+  excludes["d2105041-120b-4c06-8e61-1948a5fdf65d"] = true -- Somnambulant
+  excludes["fbedb426-410c-4614-952a-1086b6f6554f"] = true -- Brittle Bones
+  excludes["4d51ba41-2c10-4281-9308-fcfed1fe0276"] = true -- Woman in a Man's World
   
   local perks = cheat:find_perk(nil, true)
   for i,perk in pairs(perks) do
